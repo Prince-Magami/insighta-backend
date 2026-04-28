5803890a183e0bef589c149fada7298c046bedf6
Abubakar Muhammad Magami <magamiabu@gmail.com>
2026-04-28 04:01:09 -0700

    Update role.js

---
+++ middleware/role.js
@@ -0,0 +1,13 @@
+const requireRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({
+        status: "error",
+        message: "Forbidden"
+      });
+    }
+    next();
+  };
+};
+
+module.exports = { requireRole };
