3fc87052abe23f4b11ea1f4c19c07e504f0250dc
Abubakar Muhammad Magami <magamiabu@gmail.com>
2026-04-28 06:00:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -85,3 +85,23 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
+module.exports = (req, res, next) => {
+  const version = req.headers["x-api-version"];
+
+  if (!version) {
+    return res.status(400).json({
+      status: "error",
+      message: "API version header required"
+    });
+  }
+
+  if (version !== "1") {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid API version"
+    });
+  }
+
+  next();
+};
