fb2c9272bfc53e20e18977e179f496397461b815
Abubakar Muhammad Magami <magamiabu@gmail.com>
2026-04-28 04:24:29 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  github_id: String,
+  id: String,
+  github_id: { type: String, unique: true },
   username: String,
+  email: String,
+  avatar_url: String,
   role: {
     type: String,
     enum: ["admin", "analyst"],
     default: "analyst"
   },
+  is_active: {
+    type: Boolean,
+    default: true
+  },
+  last_login_at: String,
   created_at: {
     type: String,
     default: () => new Date().toISOString()
